8d092c3ab6030e0df0e50f9beb78e34ed1ce3d07
Cedar <alexianfireflies@gmail.com>
2019-08-19 18:31:06 -0700

    updated html and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var qAndA = [
+    {question: "Who created RWBY?",
+    answer1: "Monty Oum",
+    answer2: "Miles Luna",
+    answer3: "Kerry Shawcross",
+    answer4: "Rooster Teeth"},
+
+    {question: "Who are the four main characters of RWBY?",
+    answer1: "Jaune Arc, Nora Valkyrie, Pyrrha Nikos, and Lie Ren",
+    answer2: "Coco Adel, Fox Alistair, Yatsuhashi Daichi, and Velvet Scarlatina",
+    answer3: "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee",
+    answer4: "Sun Wukong, Neptune Vasilias, Scarlet David, and Sage Ayana"},
+
+    {question: "How many kingdoms are there in Remnant?",
+    answer1: "5",
+    answer2: "4",
+    answer3: "6",
+    answer4: "2"},
+
+    {question: "Who is the primary antagonist of RWBY?",
+    answer1: "Cinder Fall",
+    answer2: "Ozpin",
+    answer3: "Adam Taurus",
+    answer4: "Salem"},
+
+    {question: "What is the name of Ruby Rose's weapon?",
+    answer1: "Ember Celica",
+    answer2: "Crescent Rose",
+    answer3: "Magnhild",
+    answer4: "Miló and Akoúo̱"},
+
+    {question: "Who is the primary vocalist for RWBY?",
+    answer1: "Jeff Williams",
+    answer2: "Lamar Hall",
+    answer3: "Casey Lee Williams",
+    answer4: "Sandra Lee Casey"}
+]
