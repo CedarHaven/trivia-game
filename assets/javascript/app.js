ad7acbe24ed9a4dc87260c3267ee0f15ba486da2
Cedar <alexianfireflies@gmail.com>
2019-09-02 14:28:29 -0700

    made the answers and questions system more efficient

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,38 @@
 // question and answer object, has all the questions and the associated answers.
 var qAndA = [
     {question: "Who created RWBY?",
-    answer1: "Monty Oum",
-    answer2: "Miles Luna",
-    answer3: "Kerry Shawcross",
-    answer4: "Rooster Teeth"},
+    answers: ["Monty Oum", "Miles Luna", "Kerry Shawcross", "Rooster Teeth"],
+    correctAns: "Monty Oum"},
 
     {question: "Who are the four main characters of RWBY?",
-    answer1: "Jaune Arc, Nora Valkyrie, Pyrrha Nikos, and Lie Ren",
-    answer2: "Coco Adel, Fox Alistair, Yatsuhashi Daichi, and Velvet Scarlatina",
-    answer3: "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee",
-    answer4: "Sun Wukong, Neptune Vasilias, Scarlet David, and Sage Ayana"},
+    answers: ["Jaune Arc, Nora Valkyrie, Pyrrha Nikos, and Lie Ren", "Coco Adel, Fox Alistair, Yatsuhashi Daichi, and Velvet Scarlatina", "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee", "Sun Wukong, Neptune Vasilias, Scarlet David, and Sage Ayana"],
+    correctAns: "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee"},
 
     {question: "How many kingdoms are there in Remnant?",
-    answer1: "5",
-    answer2: "4",
-    answer3: "6",
-    answer4: "2"},
+    answers: ["5", "4", "6", "2"],
+    correctAns: "4"},
 
     {question: "Who is the primary antagonist of RWBY?",
-    answer1: "Cinder Fall",
-    answer2: "Ozpin",
-    answer3: "Adam Taurus",
-    answer4: "Salem"},
+    answers: ["Cinder Fall", "Ozpin", "Adam Taurus", "Salem"],
+    correctAns: "Salem"},
 
     {question: "What is the name of Ruby Rose's weapon?",
-    answer1: "Ember Celica",
-    answer2: "Crescent Rose",
-    answer3: "Magnhild",
-    answer4: "Miló and Akoúo̱"},
+    answers: ["Ember Celica", "Crescent Rose", "Magnhild", "Miló and Akoúo̱"],
+    correctAns: "Crescent Rose"},
 
     {question: "Who is the primary vocalist for RWBY?",
-    answer1: "Jeff Williams",
-    answer2: "Lamar Hall",
-    answer3: "Casey Lee Williams",
-    answer4: "Sandra Lee Casey"}
+    answers: ["Jeff Williams", "Lamar Hall", "Casey Lee Williams", "Sandra Lee Casey"],
+    correctAns: "Casey Lee WIlliams"}
 ]
 
 // I have a time variable (30 seconds)
 // an interval variable (for the timing stuff)
-// i (for cycling through my arrays)
-// an array of right answers in order of the questions, for checking if you got it right or not (which works because the question order isn't randomized)
+// i (for cycling through the object and making sure I'm checking the right question and stuff)
 // and counters for your wrong answers, right answers, and the questions you didn't answer at all.
 // there's also the reset button, for if you want to replay.
 var time = 30;
 var interval;
 var i = 0;
-var rightAns = ["Monty Oum", "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee", "4", "Salem", "Crescent Rose", "Casey Lee Williams"];
 var correct = 0;
 var wrong = 0;
 var timeOut = 0;
@@ -70,14 +56,11 @@ $("#start").click(startGame);
 $("#answers").on("click", function(e) {
     var text = $(e.target).text();
 
-    if(rightAns.includes(text)) {
+    if(text == qAndA[i].correctAns) {
         correct++;
         clearInterval(interval);
 
-        $("#ans-1").empty();
-        $("#ans-2").empty();
-        $("#ans-3").empty();
-        $("#ans-4").empty();
+        $("#answers").empty();
         $("#question").empty();
         $("#correct-answer").text("You got it right!");
 
@@ -88,10 +71,7 @@ $("#answers").on("click", function(e) {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#ans-1").empty();
-            $("#ans-2").empty();
-            $("#ans-3").empty();
-            $("#ans-4").empty();
+            $("#answers").empty();
             $("#question").empty();
             $("#reset").append(resetButton);
             $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
@@ -101,12 +81,9 @@ $("#answers").on("click", function(e) {
         wrong++;
         clearInterval(interval);
 
-        $("#ans-1").empty();
-        $("#ans-2").empty();
-        $("#ans-3").empty();
-        $("#ans-4").empty();
+        $("#answers").empty();
         $("#question").empty();
-        $("#correct-answer").text("Sorry, you got it wrong! The correct answer was "+rightAns[i]+".");
+        $("#correct-answer").text("Sorry, you got it wrong! The correct answer was "+qAndA[i].correctAns+".");
 
         i++;
 
@@ -114,10 +91,7 @@ $("#answers").on("click", function(e) {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#ans-1").empty();
-            $("#ans-2").empty();
-            $("#ans-3").empty();
-            $("#ans-4").empty();
+            $("#answers").empty();
             $("#question").empty();
             $("#reset").append(resetButton);
             $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
@@ -137,10 +111,13 @@ function getQuestion() {
     interval = setInterval(count, 1000);
     $("#correct-answer").empty();
     $("#question").html(qAndA[i].question);
-    $("#ans-1").html(qAndA[i].answer1);
-    $("#ans-2").html(qAndA[i].answer2);
-    $("#ans-3").html(qAndA[i].answer3);
-    $("#ans-4").html(qAndA[i].answer4);
+
+    for(var j = 0; j < qAndA[i].answers.length; j++) {
+        var newDiv = $("<div>");
+        newDiv.addClass("div-hover");
+        newDiv.text(qAndA[i].answers[j]);
+        $("#answers").append(newDiv);
+    }
 }
 
 function count() {
@@ -152,12 +129,9 @@ function count() {
         timeOut++;
         clearInterval(interval);
 
-        $("#ans-1").empty();
-        $("#ans-2").empty();
-        $("#ans-3").empty();
-        $("#ans-4").empty();
+        $("#answers").empty();
         $("#question").empty();
-        $("#correct-answer").text("You ran out of time. The answer was "+rightAns[i]+".");
+        $("#correct-answer").text("You ran out of time. The answer was "+qAndA[i].correctAns+".");
 
         i++;
 
@@ -165,10 +139,7 @@ function count() {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#ans-1").empty();
-            $("#ans-2").empty();
-            $("#ans-3").empty();
-            $("#ans-4").empty();
+            $("#answers").empty();
             $("#question").empty();
             $("#reset").append(resetButton);
             $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
