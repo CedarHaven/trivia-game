3c7bb18102ea3a56f5cef34d36bd9b78100a6e35
Cedar <alexianfireflies@gmail.com>
2019-09-22 21:19:01 -0700

    fixed game over bug, added gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,34 @@
 // question and answer object, has all the questions and the associated answers.
 var qAndA = [
-    {question: "Who created RWBY?",
+    {question: "Who is the original creator of RWBY?",
     answers: ["Monty Oum", "Miles Luna", "Kerry Shawcross", "Rooster Teeth"],
-    correctAns: "Monty Oum"},
+    correctAns: "Monty Oum",
+    imgPath: "assets/images/monty.gif"},
 
     {question: "Who are the four main characters of RWBY?",
     answers: ["Jaune Arc, Nora Valkyrie, Pyrrha Nikos, and Lie Ren", "Coco Adel, Fox Alistair, Yatsuhashi Daichi, and Velvet Scarlatina", "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee", "Sun Wukong, Neptune Vasilias, Scarlet David, and Sage Ayana"],
-    correctAns: "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee"},
+    correctAns: "Yang Xiao Long, Blake Belladonna, Ruby Rose, and Weiss Schnee",
+    imgPath: "assets/images/rwby.gif"},
 
     {question: "How many kingdoms are there in Remnant?",
     answers: ["5", "4", "6", "2"],
-    correctAns: "4"},
+    correctAns: "4",
+    imgPath: "assets/images/kingdoms.gif"},
 
     {question: "Who is the primary antagonist of RWBY?",
     answers: ["Cinder Fall", "Ozpin", "Adam Taurus", "Salem"],
-    correctAns: "Salem"},
+    correctAns: "Salem",
+    imgPath: "assets/images/salem.gif"},
 
     {question: "What is the name of Ruby Rose's weapon?",
     answers: ["Ember Celica", "Crescent Rose", "Magnhild", "Miló and Akoúo̱"],
-    correctAns: "Crescent Rose"},
+    correctAns: "Crescent Rose",
+    imgPath: "assets/images/crescent-rose.gif"},
 
     {question: "Who is the primary vocalist for RWBY?",
     answers: ["Jeff Williams", "Lamar Hall", "Casey Lee Williams", "Sandra Lee Casey"],
-    correctAns: "Casey Lee Williams"}
+    correctAns: "Casey Lee Williams",
+    imgPath: "assets/images/indomitable.gif"}
 ]
 
 // I have a time variable (30 seconds)
@@ -58,13 +64,15 @@ $("#answers").on("click", function(e) {
 
     if(text == qAndA[i].correctAns) {
         correct++;
-        console.log(correct);
-        console.log(wrong);
         clearInterval(interval);
+
+        var displayImg = $("<img>");
+        displayImg.attr("src", qAndA[i].imgPath);
 
         $("#answers").empty();
         $("#question").empty();
         $("#correct-answer").text("You got it right!");
+        $("#answer-img").append(displayImg);
 
         i++;
 
@@ -73,19 +81,20 @@ $("#answers").on("click", function(e) {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#answers").empty();
-            $("#question").empty();
-            $("#reset").append(resetButton);
-            $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
+            setTimeout(gameOver, 2000);
         }
     }
     else {
         wrong++;
         clearInterval(interval);
 
+        var displayImg = $("<img>");
+        displayImg.attr("src", qAndA[i].imgPath);
+
         $("#answers").empty();
         $("#question").empty();
         $("#correct-answer").text("Sorry, you got it wrong! The correct answer was "+qAndA[i].correctAns+".");
+        $("#answer-img").append(displayImg);
 
         i++;
 
@@ -93,16 +102,14 @@ $("#answers").on("click", function(e) {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#answers").empty();
-            $("#question").empty();
-            $("#reset").append(resetButton);
-            $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
+            setTimeout(gameOver, 2000);
         }
     }
 });
 
 // this function exists primarily so that the getQuestion function isn't clearing the start-button div every time it's called, since most of the time, that div would already be empty.
 function startGame() {
+    $("#answer-img").empty();
     $("#start-button").empty();
     getQuestion();
 }
@@ -111,7 +118,10 @@ function getQuestion() {
     time = 30;
     $("#time").html("00:30");
     interval = setInterval(count, 1000);
+
+    $("#answer-img").empty();
     $("#correct-answer").empty();
+
     $("#question").html(qAndA[i].question);
 
     for(var j = 0; j < qAndA[i].answers.length; j++) {
@@ -131,9 +141,13 @@ function count() {
         timeOut++;
         clearInterval(interval);
 
+        var displayImg = $("<img>");
+        displayImg.attr("src", qAndA[i].imgPath);
+
         $("#answers").empty();
         $("#question").empty();
         $("#correct-answer").text("You ran out of time. The answer was "+qAndA[i].correctAns+".");
+        $("#answer-img").append(displayImg);
 
         i++;
 
@@ -141,12 +155,17 @@ function count() {
             setTimeout(getQuestion, 2000);
         }
         else {
-            $("#answers").empty();
-            $("#question").empty();
-            $("#reset").append(resetButton);
-            $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
+            setTimeout(gameOver, 2000);
         }
     }
+}
+
+function gameOver() {
+    $("#answers").empty();
+    $("#question").empty();
+    $("#answer-img").empty();
+    $("#reset").append(resetButton);
+    $("#correct-answer").html("Game Over.<br>Correct Answers: "+correct+"<br>Wrong Answers: "+wrong+"<br> Unanswered: "+timeOut+"<br>If you'd like to play again, hit the reset button.");
 }
 
 // yes, I took this right out of the stopwatch. I don't care. I wanted this to look nice, and this function was right there, so I looked at it and copied it. the only thing I changed is variables, to make more sense to me/make them easier to type.
